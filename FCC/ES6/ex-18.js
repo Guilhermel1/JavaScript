b8532989bef7826ad2f087caa506fd521d25673e
GuilhermeL1 <guilhermel8686@gmail.com>
2022-05-04 23:49:54 -0300

    ex-19 concluído e ajuste de espaços dos exercícios anteriores

--- FCC/ES6/ex-18.js
+++ FCC/ES6/ex-18.js
@@ -1,6 +1,8 @@
 /* Use class Syntax to Define a Constructor Function
 
+
 Use the class keyword and write a constructor to create the Vegetable class.
+
 The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor. */
 
 // Only change code below this line
